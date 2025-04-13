fb7f984d5f0d192457dd978a0685c994b03ab354
Victor-Egas <78636948+Victor-Egas@users.noreply.github.com>
2025-04-13 00:50:16 -0500

    Delete target/classes directory

--- target/classes/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO currency (name, symbol, change_type) VALUES ('Dolares', '$', 1);
-INSERT INTO currency (name, symbol, change_type) VALUES ('Soles', 'S/', 3.784);
-INSERT INTO currency (name, symbol, change_type) VALUES ('Euros', 'E', 0.923);
